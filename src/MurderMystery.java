74cbbc06b739eeb326898e0933041e2aa558cd7d
Farheen Nashaat <farheenn@nycstudents.net>
2024-04-04 22:11:44 -0400

    murdermystery updated

--- src/MurderMystery.java
+++ src/MurderMystery.java
@@ -30,39 +30,86 @@ public class MurderMystery extends Storyline{
     public void beginning(){
         System.out.println("Ding dong! Your ex sister-in-law who\n" +
                 " is also your best friend opens the door.");
-        System.out.println("'Welcome " + super.getPlayer() +", Ashslay says teary eyed, \n" +
+        System.out.println("'Welcome " + super.getPlayer().getFirstName() +", Ashslay says teary eyed, \n" +
                 "her petite frame wracked with sobs, 'thanks for coming \n" +
                 "to this sleepover. I thought it would be a great way for us \n" +
                 "to grieve over your brother and my husband. Since he left me \n" +
                 "this huge mansion, it feels wayyy too empty and so I also invited \n" +
                 "Elislaybeth and Jesslayca.'");
 
-        System.out.println(super.getPlayer() + "It's no problem, Ashslay");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println(super.getPlayer().getFirstName() + "It's no problem, Ashslay");
         System.out.println("CHOICE: What do you want to say to Ashslay next?\n" + "1) Let's just go inside and prepare...  OR  2) Yeah I bet it's hard, having such a huggeee mansion all to yourself");
-        int choice1 = scanner.nextInt();
-        if(choice1 == 1){
-            System.out.println("Ashslay: Alright, let's go!");
-            new TicTacToe();
-        } else if(choice1 == 2){
-            System.out.println("Ashslay: How could you say that to me? You know how much your brother meant to me...");
-            System.out.println(super.getPlayer() + ": Yeah my bad. Let's just go in.");
-        } else{
-            System.out.println("invalid choice!!!");
+        int choice = super.getScanner().nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Ashslay: Alright, let's go!");
+                new TicTacToe();
+                break;
+            case 2:
+                System.out.println("Ashslay: How could you say that to me? You know how much your brother meant to me...");
+                System.out.println(super.getPlayer().getFirstName() + ": Yeah my bad. Let's just go in.");
+                break;
+            default:
+            System.out.println("Ashslay goes silent, an indescribable expression on her face.");
+            break;
         }
 
-        System.out.println(super.getPlayer() + ": 'Hey girls!'");
-        System.out.println("Elislaybeth and Jesslayca: 'Hey " + super.getPlayer() + "!', they say in unison, their crimson orbs reaching into your soul, as if they can see your deepest darkest secrets....");
-        System.out.println("Narrator: Despite the gloomy occasion, the four of you have a great time, playing multiple games.");
-        System.out.println(super.getPlayer() + ": Oh wow, time has flown by! i think we should head for bed for now.");
+        System.out.println(super.getPlayer().getFirstName() + ": 'Hey girls!'");
+        System.out.println("Elislaybeth and Jesslayca: 'Hey " + super.getPlayer().getFirstName() + "!', they say in unison, their crimson orbs reaching into your soul, as if they can see your deepest darkest secrets....");
+        System.out.println("NARRATOR: Despite the gloomy occasion, the four of you have a great time, playing multiple games.");
+        System.out.println(super.getPlayer().getFirstName() + ": Oh wow, time has flown by! i think we should head for bed for now.");
         System.out.println("Ashslay: What, no!! Let's just play one more game, hide and seek!");
         System.out.println("CHOICE: 1) Really? Hide and Seek? What are you, a kid?  OR  2)Ok, let's play.");
-        int choice2 = scanner.nextInt();
-        if(choice2 == 1){
-            System.out.println("Ashslay: 'Aww don't be like that, I just wanted to play something that brings good memories...");
-            System.out.println("Elislaybeth and Jesslayca: 'Yeah, hide and seek is fun!!', they screech in union, as they float into the air.");
-            System.out.println(super.getPlayer() + ": Alright, who's going to be the seeker?");
+
+        switch (choice){
+            case 1:
+                System.out.println("Ashslay: 'Aww don't be like that, I just wanted to play something that brings good memories...");
+                System.out.println("Elislaybeth and Jesslayca: 'Yeah, hide and seek is fun!!', they screech in union, as they float into the air.");
+                System.out.println(super.getPlayer().getFirstName() + ": Alright, who's going to be the seeker?");
+                break;
+            case 2:
+            System.out.println(super.getPlayer().getFirstName() + ": So who's going to be the seeker?");
+            break;
+            default:
+            System.out.println("invalid!!");
         }
+
+        System.out.println("Ashslay: I'll be the seeker, you guys get 4 mins to hide anywhere within this mansion. Make sure to hide like your life depends on it!!");
+        System.out.println("NARRATOR: But before that, you must change into your lucky hide and seek outfit! It goes perfectly with your " + super.getPlayer().getCharacter().getEyes()[2] + " eyes and your " + super.getPlayer().getCharacter().getHair()[0] + " hair!");
+        System.out.println(" 1) Wear this outfit" + Colors.BLUE + " 10 DIAMONDS" + Colors.RESET + " Or 2) wear your dress that is all torn up from fighting a deranged caterpillar earlier" );
+        choice = super.getScanner().nextInt();
+
+        switch (choice){
+            case 1:
+                if(super.getPlayer().getDiamonds() >= 10){
+                    super.getPlayer().spendDiamonds(10);
+                    System.out.println("You decide to wear your special hide and seek outfit, practically already declaring you winner.");
+                    System.out.println("Everyone squeals over your extraordinary choice of apparel. ");
+                } else{
+                    System.out.println("You do not have enough diamonds to wear thsi outfit!!");
+                    System.out.println("Would you like to play some mini games to acquire more?");
+                    System.out.println(Colors.RED + " CHOICE: " + Colors.RESET + "\n1) Play Wordle\n2) Play TicTacToe\n3) Play Maze\n4) Play Rock Paper Scissors\n5) Be broke then...");
+
+                    choice = super.getScanner().nextInt();
+
+                    if (choice == 5) {
+                        System.out.println("Be broke then...");
+                    } else {
+                        super.playMinigames(choice);
+                    }
+                }
+                break;
+            case 2:
+                System.out.println("You decide to wear your caterpillar mangled, torn dress.");
+                System.out.println("Ashslay tries to hide her disgust, but Elislaybeth and Jesslayca crank their heads 360 degrees towards you, throwing leeches at you in loathing.");
+                break;
+            default:
+                System.out.println("invalid, select 1 or 2");
+                break;
+        }
+
+
+
 
     }
 }
